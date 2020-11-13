bad8fe10ad5500dd15adb2e96b5362930500a96d
Ananda Wiradharma <anandawira754@gmail.com>
2020-11-13 19:51:10 +0700

    done making header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './component/Header';
+import './style/app.css';
 
 export default function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
   return (
     <div>
-      <header>
-        <Header></Header>
-      </header>
+      <Header score={score} bestScore={bestScore}></Header>
       <main></main>
     </div>
   );
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import '../style/header.css';
 
-export default function Header() {
+export default function Header(props) {
   return (
-    <div>
-      <h1>MEMORY CARD GAME</h1>
-    </div>
+    <header>
+      <div id="title">
+        <h1>MEMORY CARD GAME</h1>
+        <h3>
+          Get points by clicking on an image but don't click on any more than
+          once!
+        </h3>
+      </div>
+      <div id="score">
+        <div id="currentScore" className="scoreCard">
+          <p className='label'>SCORE</p>
+          <p className='value'>{props.score}</p>
+        </div>
+        <div id="bestScore" className="scoreCard">
+          <p className='label'>BEST</p>
+          <p className='value'>{props.bestScore}</p>
+        </div>
+      </div>
+    </header>
   );
 }
