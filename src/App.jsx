f43caf0d3b0f7887fe39272207b8e6821aaf4489
Ananda Wiradharma <anandawira754@gmail.com>
2020-11-13 18:36:12 +0700

    Add component header

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './component/Header';
+
+export default function App() {
+  return (
+    <div>
+      <header>
+        <Header></Header>
+      </header>
+      <main></main>
+    </div>
+  );
+}
---
+++ src/component/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../style/header.css';
+
+export default function Header() {
+  return (
+    <div>
+      <h1>MEMORY CARD GAME</h1>
+    </div>
+  );
+}
