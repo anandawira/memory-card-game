f80778503a973b937553c8b89cf65fa8e81a23c8
Ananda Wiradharma <anandawira754@gmail.com>
2020-11-13 22:54:34 +0700

    add assets array

---
+++ src/asset/imageIndex.js
@@ -0,0 +1,25 @@
+import image01 from './image01.jpg';
+import image02 from './image02.jpg';
+import image03 from './image03.jpg';
+import image04 from './image04.jpg';
+import image05 from './image05.jpg';
+import image06 from './image06.jpg';
+import image07 from './image07.jpg';
+import image08 from './image08.jpg';
+import image09 from './image09.jpg';
+import image10 from './image10.jpg';
+
+const images = [
+  { image: image01, clicked: false },
+  { image: image02, clicked: false },
+  { image: image03, clicked: false },
+  { image: image04, clicked: false },
+  { image: image05, clicked: false },
+  { image: image06, clicked: false },
+  { image: image07, clicked: false },
+  { image: image08, clicked: false },
+  { image: image09, clicked: false },
+  { image: image10, clicked: false },
+];
+
+export default images;
--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Card from './Card';
 import '../style/main.css';
+import images from '../asset/imageIndex';
 
 export default function Main() {
+  console.log(images);
   return (
     <main>
       <Card />
